30d710e5751a55e62d61825cae9edcfb27a0f690
Quan Nguyen <huu2uan@gmail.com>
2017-10-25 15:01:35 +0700

    cleanup

--- crawler.clj
+++ crawler.clj
@@ -3,30 +3,43 @@
          '[net.cgrand.enlive-html :refer :all])
 (import '[java.net URL])
 
+(def n (atom 0))
+
+(defn save-image [url dir]
+  (with-open [in (io/input-stream (str/replace url #" " "%20"))
+              out (io/output-stream (str dir "/" (swap! n inc) ".jpg"))]
+    (io/copy in out)))
+
 (def links (let [dom (-> "http://www.butterflycircle.com/checklist/"
                          URL. html-resource)]
              (->> (select dom [:div.butterfly-box :a])
                   (map #(get-in % [:attrs :href]))
                   set)))
 
-(defn save-image [url dir]
-  (let [name (subs url (inc (str/last-index-of url "/")))]
-    (with-open [in (io/input-stream (str/replace url #" " "%20"))
-                out (io/output-stream (str dir "/" name))]
-      (io/copy in out))))
-
-(doseq [link links]
-  (println link)
-  (let [dom (-> link URL. html-resource)
-        name (-> (select dom [:div#content-container :div.right :h1 :i])
-                 first
-                 text)
+(defn butterfly [url]
+  (let [dom (-> url URL. html-resource)
+        genus (-> (select dom [:div#content-container :div.right :table.details [:tr (nth-child 3)] [:td (nth-child 2)] :i])
+                  first
+                  text
+                  str/lower-case)
         imgs (->> (select dom [:div#gallery-thumbs :div :img])
-                  (map #(get-in % [:attrs :data-image])))
-        img-dir (str "data/" name)]
+                  (map #(get-in % [:attrs :data-image])))]
+    [genus imgs]))
 
-    (println img-dir)
-    (.mkdir (java.io.File. img-dir))
+(defn save-data []
+  (doseq [link links]
+    (println link)
+    (let [[genus imgs] (butterfly link)
+          img-dir (str "data/" genus)]
+      (.mkdir (io/file img-dir))
+      (println "save" (count imgs) "images into dir" img-dir)
+      (doall (map #(save-image % img-dir) imgs)))))
 
-    (println "save" (count imgs) "images")
-    (doall (map #(save-image % img-dir) imgs))))
+(defn cleanup []
+  (doseq [dir (->> (file-seq (io/file "data"))
+                   rest
+                   (filter #(.isDirectory %)))]
+    (when (< (count (.list dir)) 30)
+      (println "remove" (.getName dir))
+      (doseq [f (reverse (file-seq dir))]
+        (io/delete-file f)))))
