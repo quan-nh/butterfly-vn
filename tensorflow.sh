33c860e3efb90af1bc7358637259b3aa3177b415
Quan <huu2uan@gmail.com>
2017-10-25 23:54:24 +0700

    Update tensorflow.sh

--- tensorflow.sh
+++ tensorflow.sh
@@ -1,21 +1,14 @@
-# bazel https://docs.bazel.build/versions/master/install-ubuntu.html#install-on-ubuntu
+# retrain
+python tensorflow/examples/image_retraining/retrain.py --image_dir ~/projects/q/butterfly-vn/data
 
-./configure
-# build the retrainer
-bazel build --config opt tensorflow/examples/image_retraining:retrain
-# build label_image
-bazel build --config opt tensorflow/examples/image_retraining:label_image
-
-# run retrainer
-bazel-bin/tensorflow/examples/image_retraining/retrain --image_dir ~/projects/q/butterfly-vn/data
 # visualize at http://localhost:6006
 tensorboard --logdir /tmp/retrain_logs
 
 cp /tmp/output_* ~/projects/q/butterfly-vn/
 
-# run with log disable (value 0->3)
+# test
 TF_CPP_MIN_LOG_LEVEL=3 \
-bazel-bin/tensorflow/examples/image_retraining/label_image \
+python tensorflow/examples/image_retraining/label_image.py \
 --graph=/tmp/output_graph.pb --labels=/tmp/output_labels.txt \
 --output_layer=final_result:0 \
 --image=$HOME/data/231s.jpg
