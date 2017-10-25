624eb4eb9d0aa3019f0da91b3056532564853352
Quan Nguyen <huu2uan@gmail.com>
2017-10-25 19:56:22 +0700

    add image prefix

--- crawler.clj
+++ crawler.clj
@@ -5,9 +5,9 @@
 
 (def n (atom 0))
 
-(defn save-image [url dir]
+(defn save-image [url dir prefix]
   (with-open [in (io/input-stream (str/replace url #" " "%20"))
-              out (io/output-stream (str dir "/" (swap! n inc) ".jpg"))]
+              out (io/output-stream (str dir "/" prefix "_" (swap! n inc) ".jpg"))]
     (io/copy in out)))
 
 (def links (let [dom (-> "http://www.butterflycircle.com/checklist/"
@@ -22,18 +22,21 @@
                   first
                   text
                   str/lower-case)
+        species (-> (select dom [:div#content-container :div.right :table.details [:tr (nth-child 4)] [:td (nth-child 2)] :i])
+                    first
+                    text)
         imgs (->> (select dom [:div#gallery-thumbs :div :img])
                   (map #(get-in % [:attrs :data-image])))]
-    [genus imgs]))
+    [genus species imgs]))
 
 (defn save-data []
   (doseq [link links]
     (println link)
-    (let [[genus imgs] (butterfly link)
+    (let [[genus species imgs] (butterfly link)
           img-dir (str "data/" genus)]
       (.mkdir (io/file img-dir))
       (println "save" (count imgs) "images into dir" img-dir)
-      (doall (map #(save-image % img-dir) imgs)))))
+      (doall (map #(save-image % img-dir species) imgs)))))
 
 (defn cleanup []
   (doseq [dir (->> (file-seq (io/file "data"))
