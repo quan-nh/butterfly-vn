312306da35eb72e25fe8ef1b45f9244f75449817
Quan Nguyen <huu2uan@gmail.com>
2017-10-27 19:49:31 +0700

    heroku

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject messenger-bot "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.9.0-beta3"]
                  [org.clojure/core.memoize "0.5.8"]
                  [ring/ring-core "1.6.2"]
