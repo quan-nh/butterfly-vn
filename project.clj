387718fc46d73ec29a3774a2c7d56be4c4daac61
Quan Nguyen <huu2uan@gmail.com>
2017-11-01 20:13:43 +0700

    local-repo

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
+  :repositories [["local-repo" "file:repo"]]
   :dependencies [[org.clojure/clojure "1.9.0-beta3"]
                  [org.clojure/core.memoize "0.5.8"]
                  [ring/ring-core "1.6.2"]
@@ -9,7 +10,8 @@
                  [compojure "1.6.0"]
                  [http-kit "2.2.0"]
                  [org.clojure/java.jdbc "0.7.3"]
-                 [org.xerial/sqlite-jdbc "3.20.1"]]
+                 [org.xerial/sqlite-jdbc "3.20.1"]
+                 [vn.hus/nlp-tokenizer "4.1.1"]]
   :source-paths ["bot"]
   :resource-paths ["db"]
   :uberjar-name "messenger-bot-standalone.jar"
