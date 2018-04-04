f9af134ac19fbe0d50a58607215863de95ea5d32
Quan Nguyen <huu2uan@gmail.com>
2018-04-04 10:57:38 +0700

    cleanup

--- bot/handler.clj
+++ bot/handler.clj
@@ -1,7 +1,7 @@
 (ns handler
   (:require [clojure.string :as str]
             [clojure.pprint :refer [pprint]]
-            [btc] [dl] [fb] [thica]))
+            [dl] [fb]))
 
 (def verify-token (System/getenv "VERIFY_TOKEN"))
 
@@ -36,14 +36,6 @@
       (some-> nlp :entities :test first :confidence (> 0.8))
       (let [user-profile (fb/memo-user-profile sender-psid)]
         (predict-image sender-psid (:profile_pic user-profile)))
-
-      (some-> nlp :entities :bitcoin first :confidence (> 0.8))
-      (fb/send-message sender-psid
-                       {:text (btc/memo-coindesk)})
-
-      (some-> nlp :entities :thica first :confidence (> 0.8))
-      (fb/send-message sender-psid
-                       {:text (thica/memo-tai-hoa)})
 
       image-url
       (predict-image sender-psid image-url)
